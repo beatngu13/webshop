09d0a3f5d1cf0a4c1312a5fe96b54acc34218907
Joe <modulo11@users.noreply.github.com>
2016-05-27 13:30:07 +0200

    Implement category service

---
+++ categoryservice/src/main/java/de/hska/iwi/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package de.hska.iwi.controller;
+
+import de.hska.iwi.db.CategoryDAO;
+import de.hska.iwi.domain.Category;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private CategoryDAO categoryDAO = new CategoryDAO();
+
+    @RequestMapping(method = RequestMethod.GET, value = "/category")
+    public List<Category> getCategories() {
+        return categoryDAO.getObjectList();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/category/{name}")
+    public Category getCategoryByName(@PathVariable String name) {
+        return categoryDAO.getObjectByName(name);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/category")
+    public Category addCategory(@RequestBody Category request) {
+        categoryDAO.saveObject(request);
+        return categoryDAO.getObjectByName(request.getName());
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/category/{id}")
+    public void removeCategory(@PathVariable int id) {
+        categoryDAO.deleteById(id);
+    }
+
+}
---
+++ categoryservice/src/main/java/de/hska/iwi/db/HibernateUtil.java
@@ -0,0 +1,27 @@
+package de.hska.iwi.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = null;
+
+    static {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}

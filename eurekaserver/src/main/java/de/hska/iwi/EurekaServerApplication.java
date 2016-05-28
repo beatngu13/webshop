15b662616bb11e1090e68d37eb8b4f45afb6fb2e
Joe <modulo11@users.noreply.github.com>
2016-05-28 09:54:22 +0200

    Add missing files

---
+++ eurekaserver/src/main/java/de/hska/iwi/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package de.hska.iwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringCloudApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
--- productcategoryservice/src/main/java/de/hska/iwi/ProductCategoryServiceApplication.java
+++ productcategoryservice/src/main/java/de/hska/iwi/ProductCategoryServiceApplication.java
@@ -1,9 +1,13 @@
 package de.hska.iwi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class ProductCategoryServiceApplication {
 
     public static void main(String[] args) {
